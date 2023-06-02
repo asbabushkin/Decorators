332b388933ad6e32aee98f9fba928959ade9c444
asbabushkin <asbabushkin@yandex.ru>
2023-06-02 10:11:35 +0500

    block_3.3 ex.1, 2 minor improvements

--- block_3.3.py
+++ block_3.3.py
@@ -11,10 +11,13 @@ from time import sleep
 from typing import Callable
 
 CACHE = dict()
+arg_lst = [[1, 2], [2, 1], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12, 13], [14, 15, 16, 17], [1, 2], [3, 4], [1, 7, 9],
+           [99, 199], [11, 22, 33], [44, 55, 66]]
+arg_lst_sugar = [[1, 2, 3], [4, 5, 6], [11, 22, 33], [44, 55, 66]]
 
 
-def summarize(*args, **kwargs):
-    return sum(args) + sum(kwargs.values())
+def summarize(*args):
+    return sum(args)
 
 
 def clean_cache():
@@ -26,19 +29,19 @@ def clean_cache():
             cache_local[ar] = CACHE[ar]
         else:
             print(
-                f'Функция с аргументами {ar} вычислена более {timedelta.seconds} секунд назад. Данные устарели. Удаляем из кэша.')
+                f'Функция с аргументами {ar} вычислена {timedelta.seconds} секунд назад. Данные устарели. Удаляем из кэша.')
     CACHE = cache_local
     return CACHE
 
 
+# функциональный декоратор без параметров
 def decor_func(func: Callable) -> Callable:
     def inner(*args):
         global CACHE
         args_tpl = tuple(sorted(args))
+
         sleep(1)
-
         clean_cache()
-
         if args_tpl in CACHE.keys():
             return f'{CACHE[args_tpl]["result"]} получено из кэша'
         else:
@@ -52,15 +55,44 @@ def decor_func(func: Callable) -> Callable:
     return inner
 
 
-arg_lst = [[1, 2], [2, 1], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12, 13], [14, 15, 16, 17], [1, 2], [3, 4], [1, 7, 9],
-           [99, 199], [11, 22, 33], [44, 55, 66]]
 summa = decor_func(summarize)
+print('***\nРаботает функциональный декоратор без параметров!\n***')
+[print(f'Аргументы: {ar}, сумма = {summa(*ar)}') for ar in arg_lst]
+print(f'Кэш: {CACHE}')
 
-for ar in arg_lst:
-    print(f'Аргументы: {ar}, сумма = {summa(*ar)}')
 
+# сахарный декоратор без параметров:
+
+def sugar_decorator(func: Callable) -> Callable:
+    def inner(*args):
+        global CACHE
+        args_tpl = tuple(sorted(args))
+
+        sleep(1)
+        clean_cache()
+        if args_tpl in CACHE.keys():
+            return f'{CACHE[args_tpl]["result"]} получено из кэша'
+        else:
+            res = func(*args)
+            CACHE[args_tpl] = {
+                'result': res,
+                'time_calc': datetime.now()
+            }
+        return res
+
+    return inner
+
+
+@sugar_decorator
+def summarize_sugar(*args):
+    return sum(args)
+
+print('***\nРаботает сахарный декоратор без параметров!\n***')
+[print(f'Аргументы: {ar}, сумма = {summarize_sugar(*ar)}') for ar in arg_lst_sugar]
 print(f'Кэш: {CACHE}')
 
 """
 3.3 Параметризовать время кэширования в декораторе.
 """
+
+
